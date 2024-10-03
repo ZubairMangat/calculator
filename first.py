0d7581407176f71d53564e2f5ec48aa92eec15ff
zubair khalil <zubria0702@gmail.com>
2024-10-03 16:59:42 +0500

    adding multiply and divison function

--- first.py
+++ first.py
@@ -7,3 +7,13 @@ def add(x, y):
 # Function to subtract two numbers
 def subtract(x, y):
     return x - y
+# Function to multiply two numbers
+def multiply(x, y):
+    return x * y
+
+# Function to divide two numbers
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error! Division by zero."
